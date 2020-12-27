ac4af6caa1183712beebb5d31bef4c8b01bc4ac6
codingsh <developerfred@protonmail.ch>
2020-12-27 21:07:44 +0000

    start project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ditto Staking</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+export const dittoTheme = {
+  brand: {
+    900: "#ed7ac0",
+    800: "#1f1f1f",
+    700: "#fff",
+  },
+};
